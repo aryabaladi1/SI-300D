8fbf0f82163510e1c8ff937b5d3a7e92893483b9
Arya Baladi <ariabaladi.ab@gmail.com>
2026-08-31 09:34:21 +0330

    Add project files.

---
+++ SI-300D/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SI_300D
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
