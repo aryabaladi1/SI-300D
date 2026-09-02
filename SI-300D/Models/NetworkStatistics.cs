ed152f9e4366dbae5cf748bfa2566db1978bf6c3
Arya Baladi <ariabaladi.ab@gmail.com>
2026-09-02 12:09:36 +0330

    Add NetworkStatisticsService

---
+++ SI-300D/Models/NetworkStatistics.cs
@@ -0,0 +1,13 @@
+﻿namespace SI_300D.Models
+{
+    public class NetworkStatistics
+    {
+        public long BytesReceived { get; set; }
+
+        public long BytesSent { get; set; }
+
+        public double DownloadBytesPerSecond { get; set; }
+
+        public double UploadBytesPerSecond { get; set; }
+    }
+}
---
+++ SI-300D/Services/NetworkStatisticsService.cs
@@ -0,0 +1,58 @@
+﻿using SI_300D.Models;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+
+namespace SI_300D.Services
+{
+    public class NetworkStatisticsService
+    {
+        public NetworkStatistics GetStatistics(NetworkInterface networkInterface)
+        {
+            var statistics = networkInterface.GetIPStatistics();
+
+            return new NetworkStatistics
+            {
+                BytesReceived = statistics.BytesReceived,
+                BytesSent = statistics.BytesSent
+            };
+        }
+
+        public NetworkStatistics CalculateRate(NetworkStatistics previous, NetworkStatistics current, double elapsedSeconds)
+        {
+            return new NetworkStatistics
+            {
+                BytesReceived = current.BytesReceived,
+                BytesSent = current.BytesSent,
+
+                DownloadBytesPerSecond =
+                    (current.BytesReceived - previous.BytesReceived) / elapsedSeconds,
+
+                UploadBytesPerSecond =
+                    (current.BytesSent - previous.BytesSent) / elapsedSeconds
+            };
+        }
+
+        public async Task MonitorAsync(NetworkInterface networkInterface, CancellationToken cancellationToken)
+        {
+            using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+
+            var previous = GetStatistics(networkInterface);
+            var stopwatch = Stopwatch.StartNew();
+
+            while (await timer.WaitForNextTickAsync(cancellationToken))
+            {
+                var current = GetStatistics(networkInterface);
+
+                var elapsedSeconds = stopwatch.Elapsed.TotalSeconds;
+                stopwatch.Restart();
+
+                var statistics = CalculateRate(
+                    previous,
+                    current,
+                    elapsedSeconds);
+
+                previous = current;
+            }
+        }
+    }
+}
