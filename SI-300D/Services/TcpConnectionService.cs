75656f1258dfd13791a83535bed4ac42f2e4b6bc
Arya Baladi <ariabaladi.ab@gmail.com>
2026-09-05 15:00:59 +0330

    Modify TcpConnectionService

--- SI-300D/Services/TcpConnectionService.cs
+++ SI-300D/Services/TcpConnectionService.cs
@@ -1,26 +1,38 @@
 ﻿using SI_300D.Models;
+using SI_300D.Services.Windows;
 using System.Net.NetworkInformation;
 
 namespace SI_300D.Services
 {
     public class TcpConnectionService
     {
+        private readonly TcpTableService _tcpTableService;
+
+        public TcpConnectionService()
+        {
+            _tcpTableService = new TcpTableService();
+        }
+
         public List<TcpConnection> GetActiveConnections()
         {
-            var connections = IPGlobalProperties
-                .GetIPGlobalProperties()
-                .GetActiveTcpConnections();
+            var entries = _tcpTableService.GetTcpTable();
 
-            return connections
-                .Select(connection => new TcpConnection
+            var connections = new List<TcpConnection>();
+
+            foreach (var entry in entries)
+            {
+                connections.Add(new TcpConnection
                 {
-                    LocalAddress = connection.LocalEndPoint.Address.ToString(),
-                    LocalPort = connection.LocalEndPoint.Port,
-                    RemoteAddress = connection.RemoteEndPoint.Address.ToString(),
-                    RemotePort = connection.RemoteEndPoint.Port,
-                    State = connection.State
-                })
-                .ToList();
+                    LocalAddress = entry.LocalAddress,
+                    LocalPort = entry.LocalPort,
+                    RemoteAddress = entry.RemoteAddress,
+                    RemotePort = entry.RemotePort,
+                    State = (TcpState)entry.State,
+                    ProcessId = entry.ProcessId
+                });
+            }
+
+            return connections;
         }
     }
 }
