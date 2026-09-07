bf1e3bb8acb8f5af48882334bd024537cf98bdf1
Arya Baladi <ariabaladi.ab@gmail.com>
2026-09-07 17:09:14 +0330

    Add Id field to NetworkInterfaceInfo

--- SI-300D/Models/NetworkInterfaceInfo.cs
+++ SI-300D/Models/NetworkInterfaceInfo.cs
@@ -2,6 +2,8 @@
 {
     public class NetworkInterfaceInfo
     {
+        public string Id { get; set; } = string.Empty;
+
         public string Name { get; set; } = string.Empty;
 
         public string Description { get; set; } = string.Empty;
